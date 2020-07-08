425d82e13540656abda9611f2ce5ddb49ab1eb86
Reza_Atom <46934941+RezaParsian@users.noreply.github.com>
2020-07-09 00:28:43 +0430

    Add files via upload

---
+++ youtube_movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Youtube_Downloader
+{
+    class youtube_movies
+    {
+        public string url { get; set; }
+        public string quality { get; set; }
+    }
+}
